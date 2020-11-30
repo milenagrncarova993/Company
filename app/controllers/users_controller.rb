c4803876286c6c226926a3aa9a8cc1ddfb9686c5
Milena Grncarova <milenagrncarov@outlook.com>
2020-11-30 12:10:09 +0100

    Added API

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,23 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [ :show, :update ]
   def show
     @user = User.first
   end
 
   def update
+    if @restaurant.update(restaurant_params)
+      render :show
+    else
+      render_error
+    end
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address)
+  end
+
+  def render_error
+    render json: { errors: @restaurant.errors.full_messages },
+      status: :unprocessable_entity
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'profile', to: 'users#show'
 
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      resources :users, only: [ :show, :update ]
+    end
+  end
 end
